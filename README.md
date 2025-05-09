409237bbba1b5e34a04730b4c6ad9fc673b73f71
Mike Habib <nikeyn@me.com>
2025-05-09 18:58:15 +0000

    Initial commit - FastAPI scaffold for legacy Inventory app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Inventory Web App
+
+This is the modern Python-based web version of the legacy VB.NET inventory application.
---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/")
+def read_root():
+    return {"message": "Inventory Web App - Under Construction"}
